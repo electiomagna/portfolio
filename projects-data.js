0062cd10e8704afa422760e7387d046bcbcd2d21
electiomagna <electiomagna@email.com>
2026-09-17 21:51:36 +0300

    update portfolio

---
+++ projects-data.js
@@ -0,0 +1,104 @@
+const portfolioProjects = [
+  {
+    id: "senso-unico",
+    title: "Senso Unico 9a",
+    meta: "Severnaya Stena",
+    preview: "images/senso unico/0.jpg",
+    video: "video/senso unico/Senso Unico - portfolio.mp5",
+    description:
+      `Проект посвящен первопроходу Алексея Петько трассы Senso Unico, которая находится в нацпарке Лиетлахти. 
+      Трасса категории 9a. Фильм сочетает в себе элементы спортивной драмы, документальной съемки и красивой художественной хроники в стиле National Geographic. 
+      На наших глазах случаются легендарные события: где-то в лесной глуши человек испытывает свою силу, свои способности и психику на прочность. 
+      Скалолазание на естественном рельефе - это особый вид удовольствия для наблюдателей, приглашаем разделить с нами эти восторги.`,
+    gallery: [
+      "images/senso unico/1.jpg",
+      "images/senso unico/2.jpg",
+      "images/senso unico/3.jpg",
+      "images/senso unico/4.jpg",
+      "images/senso unico/5.jpg",
+      "images/senso unico/6.jpg",
+      "images/senso unico/7.jpg",
+      "images/senso unico/8.jpg",
+      "images/senso unico/9.jpg",
+      "images/senso unico/10.jpg",
+      "images/senso unico/11.jpg",
+      "images/senso unico/12.jpg",
+      "images/senso unico/13.jpg",
+      "images/senso unico/14.jpg",
+      "images/senso unico/15.jpg"
+    ]
+  },
+  {
+    id: "burgers",
+    title: "Burgers",
+    meta: "Improvisation",
+    preview: "images/Burgers/3.jpg",
+    video: "video/Burgers/Бургеры на Гриле v1.mp5",
+    description:
+      `Короткая визуальная история про приготовление бургеров на гриле. 
+      Локация - отдаленное поле. Начавшийся град нам не помешал отлично провести время и вдоволь наестся вкусными бургерами.`,
+    gallery: [
+      "images/Burgers/1.jpg",
+      "images/Burgers/2.jpg",
+      "images/Burgers/3.jpg",
+      "images/Burgers/4.jpg",
+      "images/Burgers/5.jpg",
+      "images/Burgers/6.jpg",
+      "images/Burgers/7.jpg",
+      "images/Burgers/8.jpg",
+      "images/Burgers/9.jpg",
+      "images/Burgers/10.jpg",
+      "images/Burgers/11.jpg"
+    ]
+  },
+  {
+    id: "chatarsis",
+    title: "Chatarsis 8c+",
+    meta: "Triangular lake",
+    preview: "images/chatarsis/0.jpg",
+    video: "video/chatarsis/catarsis5 - 4K.mov5",
+    description:
+      `Скалолазный проект на Треугольном озере: Chatarsis 8c+ - культовая трасса Треугольного озера. 
+      Ее проходит Владислав Шевченко - один из сильнейших спортсменов страны.`,
+    gallery: [
+      "images/chatarsis/1.jpg",
+      "images/chatarsis/2.jpg",
+      "images/chatarsis/3.jpg",
+      "images/chatarsis/4.jpg",
+      "images/chatarsis/5.jpg",
+      "images/chatarsis/6.jpg",
+      "images/chatarsis/7.jpg",
+      "images/chatarsis/8.jpg",
+      "images/chatarsis/9.jpg"
+    ]
+  },
+  {
+    id: "elcap",
+    title: "El Capitan 5x5",
+    meta: "El Capitan climbing gym",
+    preview: "images/elcap_1/0.jpg",
+    video: "video/elcap_1/elcap final7.mp5",
+    description:
+      `Проект для скалодрома El Capitan: энергичная съемка соревнований по болдерингу. 
+      Ребята из ElCapitan'а ежегодно проводят фестивали, этот - в честь дня рождения скалодрому - 7 лет. 
+      Это всегда очень зрелищно, драйвово, шумно и весело. Очень рад быть частью этого комьюнити.`,
+    gallery: [
+      "images/elcap_1/0.jpg",
+      "images/elcap_1/1.jpg",
+      "images/elcap_1/2.jpg",
+      "images/elcap_1/3.jpg",
+      "images/elcap_1/4.jpg",
+      "images/elcap_1/5.jpg",
+      "images/elcap_1/6.jpg",
+      "images/elcap_1/7.jpg",
+      "images/elcap_1/8.jpg",
+      "images/elcap_1/9.jpg",
+      "images/elcap_1/10.jpg",
+      "images/elcap_1/11.jpg",
+      "images/elcap_1/12.jpg",
+      "images/elcap_1/13.jpg",
+      "images/elcap_1/14.jpg",
+      "images/elcap_1/15.jpg"
+    ]
+  }
+];
